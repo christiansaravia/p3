ef971f354cbaf10d9e76558e2fffe300d30dbdef
Christian Saravia <christian@cs50.harvard.edu>
2016-10-24 21:08:44 -0400

    fixes names of controllers

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/lipsum-generator', 'LipsumController@index')->name('lipsum-generator.index');
-Route::get('/lipsum-generator/create', 'LipsumController@create')->name('lipsum-generator.create');
-Route::post('/lipsum-generator', 'LipsumController@store')->name('lipsum-generator.store');
-Route::get('/lipsum-generator/{book}', 'LipsumController@show')->name('lipsum-generator.show');
-Route::get('/lipsum-generator/{book}/edit', 'LipsumController@edit')->name('lipsum-generator.edit');
-Route::put('/lipsum-generator/{book}', 'LipsumController@update')->name('lipsum-generator.update');
-Route::delete('/lipsum-generator/{book}', 'LipsumController@destroy')->name('lipsum-generator.destroy');
+Route::get('/lipsum-generator', 'user-generator@index')->name('lipsum-generator.index');
+Route::get('/lipsum-generator/create', 'LipsumGeneratorController@create')->name('lipsum-generator.create');
+Route::post('/lipsum-generator', 'LipsumGeneratorController@store')->name('lipsum-generator.store');
+Route::get('/lipsum-generator/{book}', 'LipsumGeneratorController@show')->name('lipsum-generator.show');
+Route::get('/lipsum-generator/{book}/edit', 'LipsumGeneratorController@edit')->name('lipsum-generator.edit');
+Route::put('/lipsum-generator/{book}', 'LipsumGeneratorController@update')->name('lipsum-generator.update');
+Route::delete('/lipsum-generator/{book}', 'LipsumGeneratorController@destroy')->name('lipsum-generator.destroy');
 
-Route::get('/user-generator', 'UserController@index')->name('user-generator.index');
-Route::get('/user-generator/create', 'UserController@create')->name('user-generator.create');
-Route::post('/user-generator', 'UserController@store')->name('user-generator.store');
-Route::get('/user-generator/{book}', 'UserController@show')->name('user-generator.show');
-Route::get('/user-generator/{book}/edit', 'UserController@edit')->name('user-generator.edit');
-Route::put('/user-generator/{book}', 'UserController@update')->name('user-generator.update');
-Route::delete('/user-generator/{book}', 'UserController@destroy')->name('user-generator.destroy');
+Route::get('/user-generator', 'UserGeneratorController@index')->name('user-generator.index');
+Route::get('/user-generator/create', 'UserGeneratorController@create')->name('user-generator.create');
+Route::post('/user-generator', 'UserGeneratorController@store')->name('user-generator.store');
+Route::get('/user-generator/{book}', 'UserGeneratorController@show')->name('user-generator.show');
+Route::get('/user-generator/{book}/edit', 'UserGeneratorController@edit')->name('user-generator.edit');
+Route::put('/user-generator/{book}', 'UserGeneratorController@update')->name('user-generator.update');
+Route::delete('/user-generator/{book}', 'UserGeneratorController@destroy')->name('user-generator.destroy');
