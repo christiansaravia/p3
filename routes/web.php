07878222df984e9ea0dc41f66d507d4e3814f8a2
Christian Saravia <christian@cs50.harvard.edu>
2016-10-24 21:04:11 -0400

    updates readme; adds routes and controllers for UserGenerator and LipsumGenerator

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lipsum-generator', 'LipsumController@index')->name('lipsum-generator.index');
+Route::get('/lipsum-generator/create', 'LipsumController@create')->name('lipsum-generator.create');
+Route::post('/lipsum-generator', 'LipsumController@store')->name('lipsum-generator.store');
+Route::get('/lipsum-generator/{book}', 'LipsumController@show')->name('lipsum-generator.show');
+Route::get('/lipsum-generator/{book}/edit', 'LipsumController@edit')->name('lipsum-generator.edit');
+Route::put('/lipsum-generator/{book}', 'LipsumController@update')->name('lipsum-generator.update');
+Route::delete('/lipsum-generator/{book}', 'LipsumController@destroy')->name('lipsum-generator.destroy');
+
+Route::get('/user-generator', 'UserController@index')->name('user-generator.index');
+Route::get('/user-generator/create', 'UserController@create')->name('user-generator.create');
+Route::post('/user-generator', 'UserController@store')->name('user-generator.store');
+Route::get('/user-generator/{book}', 'UserController@show')->name('user-generator.show');
+Route::get('/user-generator/{book}/edit', 'UserController@edit')->name('user-generator.edit');
+Route::put('/user-generator/{book}', 'UserController@update')->name('user-generator.update');
+Route::delete('/user-generator/{book}', 'UserController@destroy')->name('user-generator.destroy');
